ec43fcbb3c9ed2cb75449e07d1b9968f605835cd
deebloo <dannybluedesign@gmail.com>
2023-09-24 20:48:50 -0400

    optional rounding

--- src/length/units/inches.rs
+++ src/length/units/inches.rs
@@ -4,8 +4,8 @@ impl Length {
     pub fn as_inches(&self) -> Self {
         match self {
             Self::In(_) => self.round(),
-            Self::Ft(val) => Self::In(val * 12.).round(),
-            Self::Yd(val) => Self::In(val * 36.).round(),
+            Self::Ft(val) => Self::In(val * 12.),
+            Self::Yd(val) => Self::In(val * 36.),
         }
     }
 }
--- src/length/units/yards.rs
+++ src/length/units/yards.rs
@@ -3,9 +3,9 @@ use crate::Length;
 impl Length {
     pub fn as_yards(&self) -> Self {
         match self {
-            Self::In(val) => Self::Yd(val / 36.).round(),
-            Self::Ft(val) => Self::Yd(val / 3.).round(),
-            Self::Yd(_) => self.round(),
+            Self::In(val) => Self::Yd(val / 36.),
+            Self::Ft(val) => Self::Yd(val / 3.),
+            Self::Yd(_) => *self,
         }
     }
 }

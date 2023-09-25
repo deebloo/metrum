b75db1e4f0e1219b41b0fe3e537b1155e53bfc5a
deebloo <dannybluedesign@gmail.com>
2023-09-24 20:51:19 -0400

    length.sub

---
+++ src/length/ops/sub.rs
@@ -0,0 +1,26 @@
+use crate::length::Length;
+use std::ops::Sub;
+
+impl Sub for Length {
+    type Output = Length;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        match self {
+            Self::In(val) => {
+                let target: f64 = rhs.as_inches().into();
+
+                Self::In(val - target)
+            }
+            Self::Ft(val) => {
+                let target: f64 = rhs.as_feet().into();
+
+                Self::Ft(val - target)
+            }
+            Self::Yd(val) => {
+                let target: f64 = rhs.as_yards().into();
+
+                Self::Yd(val - target)
+            }
+        }
+    }
+}

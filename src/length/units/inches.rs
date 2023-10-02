60bf684a83ea7e038c5a4b2126d3918bc2eb6e57
deebloo <dannybluedesign@gmail.com>
2023-10-02 12:02:03 -0400

    I am effing blind

--- src/length/units/inches.rs
+++ src/length/units/inches.rs
@@ -3,10 +3,10 @@ use crate::Length;
 impl Length {
     pub fn as_inches(&self) -> Self {
         match self {
-            Self::In(_) => self.round(),
+            Self::In(_) => *self,
             Self::Ft(val) => Self::In(val * 12.),
             Self::Yd(val) => Self::In(val * 36.),
-            Self::Mi(val) => Self::Mi(val / 5280.),
+            Self::Mi(val) => Self::In(val / 63360.),
         }
     }
 }
--- src/length/units/yards.rs
+++ src/length/units/yards.rs
@@ -6,7 +6,7 @@ impl Length {
             Self::In(val) => Self::Yd(val / 36.),
             Self::Ft(val) => Self::Yd(val / 3.),
             Self::Yd(_) => *self,
-            Self::Mi(val) => Self::Mi(val / 1760.),
+            Self::Mi(val) => Self::Yd(val * 1760.),
         }
     }
 }

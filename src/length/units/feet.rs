5885d14718d2531bc765185e32d8f9bbdf23b80a
deebloo <dannybluedesign@gmail.com>
2023-10-07 23:28:56 -0400

    feet conversion test

--- src/length/units/feet.rs
+++ src/length/units/feet.rs
@@ -11,3 +11,11 @@ impl Length {
         }
     }
 }
+
+#[test]
+fn should_convert_to_feet() {
+    assert_eq!(Length::Ft(1.), Length::In(12.).as_feet());
+    assert_eq!(Length::Ft(3.), Length::Yd(1.).as_feet());
+    assert_eq!(Length::Ft(5280.), Length::Mi(1.).as_feet());
+    assert_eq!(Length::Ft(1.), Length::MM(304.8).as_feet());
+}

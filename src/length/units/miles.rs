77bf0ede002ef6969f01ab1f47175d4d565b36ea
deebloo <dannybluedesign@gmail.com>
2023-10-09 09:18:32 -0400

    miles conversion test

--- src/length/units/miles.rs
+++ src/length/units/miles.rs
@@ -11,3 +11,11 @@ impl Length {
         }
     }
 }
+
+#[test]
+fn should_convert_to_feet() {
+    assert_eq!(Length::Mi(1.), Length::In(63360.).as_miles());
+    assert_eq!(Length::Mi(1.), Length::Ft(5280.).as_miles());
+    assert_eq!(Length::Mi(1.), Length::Yd(1760.).as_miles());
+    assert_eq!(Length::Mi(1.), Length::MM(1.609e+6).as_miles());
+}

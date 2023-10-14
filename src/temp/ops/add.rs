7bd1bcdac50f4764771576113b09e5009e8d1215
deebloo <dannybluedesign@gmail.com>
2023-10-14 17:14:55 -0400

    allow you to add f64 directly to Temp

--- src/temp/ops/add.rs
+++ src/temp/ops/add.rs
@@ -1,7 +1,7 @@
 use crate::temp::Temp;
 use std::ops::Add;
 
-impl Add for Temp {
+impl Add<Self> for Temp {
     type Output = Temp;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -25,6 +25,18 @@ impl Add for Temp {
     }
 }
 
+impl Add<f64> for Temp {
+    type Output = Temp;
+
+    fn add(self, rhs: f64) -> Self::Output {
+        match self {
+            Self::C(val) => Self::C(val + rhs),
+            Self::F(val) => Self::F(val + rhs),
+            Self::K(val) => Self::K(val + rhs),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,10 +46,12 @@ mod tests {
         let cc = Temp::C(10.) + Temp::C(10.);
         let cf = Temp::C(10.) + Temp::F(50.);
         let ck = Temp::C(10.) + Temp::K(283.15);
+        let cf64 = Temp::C(10.) + 10.;
 
         assert_eq!(cc, Temp::C(20.));
         assert_eq!(cf, Temp::C(20.));
         assert_eq!(ck, Temp::C(20.));
+        assert_eq!(cf64, Temp::C(20.));
     }
 
     #[test]
@@ -45,10 +59,12 @@ mod tests {
         let ff = Temp::F(100.) + Temp::F(100.);
         let fc = Temp::F(100.) + Temp::C(37.778);
         let fk = Temp::F(100.) + Temp::K(310.928);
+        let ff64 = Temp::F(10.) + 10.;
 
         assert_eq!(ff, Temp::F(200.));
         assert_eq!(fc, Temp::F(200.0004));
         assert_eq!(fk, Temp::F(200.00040000000004));
+        assert_eq!(ff64, Temp::F(20.));
     }
 
     #[test]
@@ -56,9 +72,11 @@ mod tests {
         let kk = Temp::K(300.) + Temp::K(300.);
         let kc = Temp::K(300.) + Temp::C(26.85);
         let kf = Temp::K(300.) + Temp::F(80.33);
+        let kf64 = Temp::K(10.) + 10.;
 
         assert_eq!(kk, Temp::K(600.));
         assert_eq!(kf, Temp::K(600.));
         assert_eq!(kc, Temp::K(600.));
+        assert_eq!(kf64, Temp::K(20.));
     }
 }
